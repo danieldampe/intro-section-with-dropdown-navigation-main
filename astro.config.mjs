421eee52f2ba063e47344e5510df8bdf92ba15cc
danieldampe <damper.daniel.sm@gmail.com>
2024-08-03 14:12:00 -0500

    fixing base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danieldampe.github.io',
-  base: '/intro-section-with-dropdown-navigation-main',
+  base: 'intro-section-with-dropdown-navigation-main',
   integrations: [tailwind()]
 })
